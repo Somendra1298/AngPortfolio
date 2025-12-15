78981c6a42f56d12c9f33beab010f67c52f159e1
Somendra Tripathi <94559291+Somendra1298@users.noreply.github.com>
2025-12-15 21:55:59 +0530

    Update app.component.ts
    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
+
 import { PrimeNgModule } from '../shared/prime-ng.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ToastMessageComponent } from "./components/toast-message/toast-message.component";
-
+import { ToastMessageComponent } from './components/toast-message/toast-message.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PrimeNgModule, NavbarComponent, ToastMessageComponent],
+  imports: [
+    RouterOutlet,
+    PrimeNgModule,
+    NavbarComponent,
+    ToastMessageComponent
+  ],
+  providers: [MessageService],   // ✅ IMPORTANT
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
