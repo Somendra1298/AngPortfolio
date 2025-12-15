39cd56ff033a97e2f1a49cfc0e6817cb165a1af7
Somendra Tripathi <94559291+Somendra1298@users.noreply.github.com>
2025-12-15 21:49:46 +0530

    Update toast-message.component.ts
    new changes on toast

--- src/app/components/toast-message/toast-message.component.ts
+++ src/app/components/toast-message/toast-message.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ToastModule } from 'primeng/toast';
-import { MessageService } from 'primeng/api';
-import { SharedService, ToastMessage } from '../../../shared/shared.service';
 
 @Component({
   selector: 'app-toast-message',
   standalone: true,
   imports: [ToastModule],
-  template: `<p-toast></p-toast>`,
-  providers: [MessageService]
+  template: `<p-toast position="top-right"></p-toast>`
 })
-export class ToastMessageComponent implements OnInit {
-
-  constructor(
-    private messageService: MessageService,
-    private sharedService: SharedService
-  ) {}
-
-  ngOnInit(): void {
-    this.sharedService.toast$.subscribe((toast: ToastMessage) => {
-      this.messageService.add(toast);
-    });
-  }
-}
+export class ToastMessageComponent {}
